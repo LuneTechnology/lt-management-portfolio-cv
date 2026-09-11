74f0c545350a7216cf4ba4fcd593a8e8c4c2251a
BiyanSudarto <biyansudarto05@gmail.com>
2026-09-11 23:49:34 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/products', [ProductController::class, 'index']);
+Route::apiResource('roles', RoleController::class);
